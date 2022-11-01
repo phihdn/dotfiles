b6c0a63a81a362cf97d91b55ec9a077049040998
Phi <phihdn@gmail.com>
2022-11-01 20:36:16 +0700

    update indent

--- nvim/conf/lua/phihdn/plugins/onenord.lua
+++ nvim/conf/lua/phihdn/plugins/onenord.lua
@@ -1,12 +1,12 @@
 -- import comment plugin safely
 local setup, onenord = pcall(require, "onenord")
 if not setup then
-	return
+    return
 end
 
 onenord.setup({
-  theme = "dark",
-  disable = {
-    background = true
-  }
+    theme = "dark",
+    disable = {
+        background = true
+    }
 })
