77a9813e4ab8f06011a4ba1a9f0b715be9299f79
Phi <phihdn@gmail.com>
2022-11-01 20:36:16 +0700

    update indent

--- nvim/conf/lua/phihdn/plugins/lualine.lua
+++ nvim/conf/lua/phihdn/plugins/lualine.lua
@@ -1,7 +1,7 @@
 -- import lualine plugin safely
 local status, lualine = pcall(require, "lualine")
 if not status then
-	return
+    return
 end
 
 -- get lualine onenord theme
@@ -9,11 +9,11 @@ local lualine_onenord = require("lualine.themes.onenord")
 
 -- new colors for theme
 local new_colors = {
-	blue = "#65D1FF",
-	green = "#3EFFDC",
-	violet = "#FF61EF",
-	yellow = "#FFDA7B",
-	black = "#000000",
+    blue = "#65D1FF",
+    green = "#3EFFDC",
+    violet = "#FF61EF",
+    yellow = "#FFDA7B",
+    black = "#000000",
 }
 
 -- change onenord theme colors
@@ -21,17 +21,17 @@ lualine_onenord.normal.a.bg = new_colors.blue
 lualine_onenord.insert.a.bg = new_colors.green
 lualine_onenord.visual.a.bg = new_colors.violet
 lualine_onenord.command = {
-	a = {
-		gui = "bold",
-		bg = new_colors.yellow,
-		fg = new_colors.black, -- black
-	},
+    a = {
+        gui = "bold",
+        bg = new_colors.yellow,
+        fg = new_colors.black, -- black
+    },
 }
 
 -- configure lualine with modified theme
 lualine.setup({
-	options = {
-    icons_enabled = true,
-		theme = lualine_onenord,
-	},
+    options = {
+        icons_enabled = true,
+        theme = lualine_onenord,
+    },
 })
