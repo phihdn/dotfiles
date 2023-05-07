cae2fa1bb56653403c44675b13edcb09c08a9a0a
Phi <phihdn@gmail.com>
2023-05-07 20:50:19 +0700

    add alias for git status

--- fish/alias.fish
+++ fish/alias.fish
@@ -3,11 +3,14 @@
 #alias la="ls -A"
 #alias ll="ls -l"
 #alias lla="ll -A"
-alias g "git"
 if type -q exa
-  alias ls "exa --icons --group-directories-first"
-  alias ll "exa --icons --group-directories-first -l"
+    alias ls "exa --icons --group-directories-first"
+    alias ll "exa --icons --group-directories-first -l"
 end
 alias lla "ll -a"
 alias grep "grep --color"
-command -qv nvim && alias vim="nvim"
+command -qv nvim && alias vim nvim
+alias s "source ~/.config/fish/config.fish"
+
+# GIT ALIASES -----------------------------------------------------------------
+alias gs "git status"
