046602490dc5e7140f51ac9e71f32eec9039922c
Phi Hoang <1890939+phihdn@users.noreply.github.com>
2023-05-07 22:30:22 +0700

    Create testfont.sh

---
+++ bin/testfont.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+#https://www.online-toolz.com/tools/text-unicode-entities-convertor.php
+
+echo "Normal"
+echo "\033[1mBold\033[22m"
+echo "\033[3mItalic\033[23m"
+echo "\033[3;1mBold Italic\033[0m"
+echo "\033[4mUnderline\033[24m"
+echo "== === !== >= <= =>"
+echo "󰐊     󰄉      󰑓 󰒲   "
+echo " " # nf-dev-git_branch \ue725 e725
+echo "󰘬 " # nf-md-source_branch \udb81\ude2c f062c
+echo " " # nf-cod-repo_push \ueb41 eb41
+echo " " # nf-cod-repo_pull \ueb40 eb40
+echo "󰔧 " # nf-md-tooltip_plus_outline \udb81\udd27 f0527
+echo "󰕚 " # nf-md-vector_difference \udb81\udd5a f055a
+echo " " # nf-cod-new_file \uea7f ea7f
+echo " " # nf-oct-file_diff \uf4d2 f4d2
+echo " " # nf-cod-package \ueb29 eb29
+echo " " # nf-cod-diff_added \ueadc eadc
+echo " " # nf-cod-diff_removed \ueadf eadf
