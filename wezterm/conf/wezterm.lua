4ad61d1c16e780b81d522e426ceffbb244936e0d
Phi <phihdn@gmail.com>
2024-01-06 16:24:34 -0400

    update wezterm

--- wezterm/conf/wezterm.lua
+++ wezterm/conf/wezterm.lua
@@ -1,40 +1,161 @@
--- Pull in the wezterm API
+--
+-- ██╗    ██╗███████╗███████╗████████╗███████╗██████╗ ███╗   ███╗
+-- ██║    ██║██╔════╝╚══███╔╝╚══██╔══╝██╔════╝██╔══██╗████╗ ████║
+-- ██║ █╗ ██║█████╗    ███╔╝    ██║   █████╗  ██████╔╝██╔████╔██║
+-- ██║███╗██║██╔══╝   ███╔╝     ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║
+-- ╚███╔███╔╝███████╗███████╗   ██║   ███████╗██║  ██║██║ ╚═╝ ██║
+--  ╚══╝╚══╝ ╚══════╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝
+-- A GPU-accelerated cross-platform terminal emulator
+-- https://wezfurlong.org/wezterm/
+
+local k = require("utils/keys")
+
 local wezterm = require("wezterm")
+local act = wezterm.action
 
--- This table will hold the configuration.
-local config = {}
+local config = {
+	-- font
+	font = wezterm.font_with_fallback({
+		"JetBrainsMono Nerd Font",
+		{ family = "Symbols Nerd Font Mono" },
+		-- { family = "Symbols Nerd Font Mono", scale = 1.0 },
+	}),
+	font_size = 16,
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
+	-- colors
+	color_scheme = "Catppuccin Macchiato",
 
-config.color_scheme = "Catppuccin Macchiato"
+	-- padding
+	--window_padding = {
+	--	left = 20,
+	--	right = 20,
+	--	top = 20,
+	--	bottom = 10,
+	--},
 
-config.font = wezterm.font_with_fallback({
-	"JetBrainsMono Nerd Font",
-	{ family = "Symbols Nerd Font Mono", scale = 1.0 },
-})
+	set_environment_variables = {
+		-- THEME_FLAVOUR = "latte",
+		--BAT_THEME = h.is_dark() and "Catppuccin-mocha" or "Catppuccin-latte",
+	},
 
-config.window_background_opacity = 0.7
-config.macos_window_background_blur = 0
-config.force_reverse_video_cursor = true
-config.window_decorations = "RESIZE"
-config.use_cap_height_to_scale_fallback_fonts = true
-config.font_size = 20
-config.scrollback_lines = 5000
-config.hide_tab_bar_if_only_one_tab = true
-config.window_close_confirmation = "NeverPrompt"
-config.window_padding = { left = 14, right = 14, top = 10, bottom = 10 }
-config.send_composed_key_when_left_alt_is_pressed = false
-config.send_composed_key_when_right_alt_is_pressed = true
-config.adjust_window_size_when_changing_font_size = false
-config.warn_about_missing_glyphs = true
+	-- general options
+	adjust_window_size_when_changing_font_size = false,
+	debug_key_events = false,
+	enable_tab_bar = false,
+	native_macos_fullscreen_mode = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE",
 
-config.keys = {
-	{ mods = "CTRL", key = "V", action = wezterm.action.PasteFrom("Clipboard") },
+	-- keys
+	keys = {
+		k.cmd_key(".", k.multiple_actions(":ZenMode")),
+		k.cmd_key("[", act.SendKey({ mods = "CTRL", key = "o" })),
+		k.cmd_key("]", act.SendKey({ mods = "CTRL", key = "i" })),
+		k.cmd_key("f", k.multiple_actions(":Grep")),
+		k.cmd_key("H", act.SendKey({ mods = "CTRL", key = "h" })),
+		k.cmd_key("i", k.multiple_actions(":SmartGoTo")),
+		k.cmd_key("J", act.SendKey({ mods = "CTRL", key = "j" })),
+		k.cmd_key("K", act.SendKey({ mods = "CTRL", key = "k" })),
+		k.cmd_key("L", act.SendKey({ mods = "CTRL", key = "l" })),
+		k.cmd_key("P", k.multiple_actions(":GoToCommand")),
+		k.cmd_key("p", k.multiple_actions(":GoToFile")),
+		k.cmd_key("q", k.multiple_actions(":qa!")),
+		k.cmd_to_tmux_prefix("1", "1"),
+		k.cmd_to_tmux_prefix("2", "2"),
+		k.cmd_to_tmux_prefix("3", "3"),
+		k.cmd_to_tmux_prefix("4", "4"),
+		k.cmd_to_tmux_prefix("5", "5"),
+		k.cmd_to_tmux_prefix("6", "6"),
+		k.cmd_to_tmux_prefix("7", "7"),
+		k.cmd_to_tmux_prefix("8", "8"),
+		k.cmd_to_tmux_prefix("9", "9"),
+		k.cmd_to_tmux_prefix("`", "n"),
+		k.cmd_to_tmux_prefix("b", "B"),
+		k.cmd_to_tmux_prefix("C", "C"),
+		k.cmd_to_tmux_prefix("d", "D"),
+		k.cmd_to_tmux_prefix("G", "G"),
+		k.cmd_to_tmux_prefix("g", "g"),
+		k.cmd_to_tmux_prefix("j", "O"),
+		k.cmd_to_tmux_prefix("k", "T"),
+		k.cmd_to_tmux_prefix("l", "L"),
+		k.cmd_to_tmux_prefix("n", '"'),
+		k.cmd_to_tmux_prefix("N", "%"),
+		k.cmd_to_tmux_prefix("o", "u"),
+		k.cmd_to_tmux_prefix("T", "!"),
+		k.cmd_to_tmux_prefix("t", "c"),
+		k.cmd_to_tmux_prefix("w", "x"),
+		k.cmd_to_tmux_prefix("z", "z"),
+
+		k.cmd_key(
+			"R",
+			act.Multiple({
+				act.SendKey({ key = "\x1b" }), -- escape
+				k.multiple_actions(":source %"),
+			})
+		),
+
+		k.cmd_key(
+			"s",
+			act.Multiple({
+				act.SendKey({ key = "\x1b" }), -- escape
+				k.multiple_actions(":w"),
+			})
+		),
+
+		{
+			mods = "CMD|SHIFT",
+			key = "}",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "Space" }),
+				act.SendKey({ key = "n" }),
+			}),
+		},
+		{
+			mods = "CMD|SHIFT",
+			key = "{",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "Space" }),
+				act.SendKey({ key = "p" }),
+			}),
+		},
+
+		{
+			mods = "CTRL",
+			key = "Tab",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "Space" }),
+				act.SendKey({ key = "n" }),
+			}),
+		},
+
+		{
+			mods = "CTRL|SHIFT",
+			key = "Tab",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "Space" }),
+				act.SendKey({ key = "n" }),
+			}),
+		},
+
+		-- FIX: disable binding
+		-- {
+		-- 	mods = "CMD",
+		-- 	key = "`",
+		-- 	action = act.Multiple({
+		-- 		act.SendKey({ mods = "CTRL", key = "Space" }),
+		-- 		act.SendKey({ key = "n" }),
+		-- 	}),
+		-- },
+
+		{
+			mods = "CMD",
+			key = "~",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "Space" }),
+				act.SendKey({ key = "p" }),
+			}),
+		},
+	},
 }
 
--- and finally, return the configuration to wezterm
 return config
