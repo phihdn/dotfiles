267201043dfc92ac45dac67a047ee2c671010407
Phi <phihdn@gmail.com>
2024-02-23 16:41:07 -0400

    update nvim config

--- nvim/conf/lua/phihdn/plugins/nvim-lint.lua
+++ nvim/conf/lua/phihdn/plugins/nvim-lint.lua
@@ -1,18 +1,27 @@
 return {
   "mfussenegger/nvim-lint",
   lazy = true,
-  event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+  event = { "BufWritePost", "BufReadPost", "InsertLeave" },
   config = function()
     local lint = require("lint")
 
     lint.linters_by_ft = {
       go = { "golangcilint" },
+      fish = { "fish" },
       javascript = { "eslint_d" },
       typescript = { "eslint_d" },
       javascriptreact = { "eslint_d" },
       typescriptreact = { "eslint_d" },
       svelte = { "eslint_d" },
       python = { "pylint" },
+      lua = { "luacheck" },
+      sh = { "shellcheck" },
+      markdown = { "markdownlint" },
+      yaml = { "yamllint" },
+      json = { "jsonlint" },
+      toml = { "tomllint" },
+      rust = { "cargo" },
+      ruby = { "rubocop" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
@@ -23,9 +32,5 @@ return {
         lint.try_lint()
       end,
     })
-
-    vim.keymap.set("n", "<leader>l", function()
-      lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
   end,
 }
