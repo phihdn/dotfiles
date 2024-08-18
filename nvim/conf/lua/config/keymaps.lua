34be0ad895780251f891ab3c76c7a9bd76a79a7c
Phi <phihdn@gmail.com>
2024-08-17 22:48:24 -0300

    update nvim config

--- nvim/conf/lua/config/keymaps.lua
+++ nvim/conf/lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local opts = { noremap = true, silent = true }
+local map = vim.keymap.set
+
+-- Exit on jj and jk
+map("i", "jj", "<ESC>", opts)
+map("i", "jk", "<ESC>", opts)
+
+-- paste over currently selected text without yanking it
+map("v", "p", '"_dp') -- p puts text after the cursor
+map("v", "P", '"_dP') -- P puts text before the cursor
+
+map({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy to clipboard" })
+map("n", "<leader>Y", [["+Y]], { desc = "Copy line to clipboard" })
+
+-- delete single character without copying into register
+map("n", "x", '"_x', opts)
--- nvim/conf/lua/config/options.lua
+++ nvim/conf/lua/config/options.lua
@@ -3,3 +3,11 @@
 -- Add any additional options here
 vim.opt.scrolloff = 10 -- start scrolling when the cursor is 10 lines away from the edge
 vim.opt.sidescrolloff = 10 -- start scrolling when the cursor is 10 lines away from the side
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+vim.opt.clipboard = "" -- don't use system clipboard
