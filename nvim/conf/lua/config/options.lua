1139337edb3b30f729ac13b32a60e461299436ea
Phi <phihdn@gmail.com>
2024-08-17 22:46:36 -0300

    set tmux-256color and add tmux url plugin

--- nvim/conf/lua/config/options.lua
+++ nvim/conf/lua/config/options.lua
@@ -1,3 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.scrolloff = 10 -- start scrolling when the cursor is 10 lines away from the edge
+vim.opt.sidescrolloff = 10 -- start scrolling when the cursor is 10 lines away from the side
