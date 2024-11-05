308e59c51085f68cc20d343666a246769ea3e83e
Phi <phihdn@gmail.com>
2024-11-05 13:30:26 -0400

    auto install homebrew if needed

--- _install/brew.sh
+++ _install/brew.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+if ! command -v brew &>/dev/null; then
+  echo "Installing Homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+echo "Installing homebrew packages..."
+
 brew bundle install --file=$HOME/dotfiles/homebrew/Phi.Brewfile
+
