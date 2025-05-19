c6429d5b7d4f68fbd0bece05aa684b31f4bdbcd8
Phi <phihdn@gmail.com>
2025-05-19 17:44:00 -0300

    NVIM conform only format js and ts if .prettierrc.json exists.

--- nvim/conf/lua/plugins/conform.lua
+++ nvim/conf/lua/plugins/conform.lua
@@ -4,21 +4,62 @@ return {
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
-      -- Disable autoformat on certain filetypes
-      local ignore_filetypes = { "typescript", "javascript" }
-      if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
-        return
-      end
-      -- Disable with a global or buffer-local variable
+      local bufname = vim.api.nvim_buf_get_name(bufnr)
+      local filetype = vim.bo[bufnr].filetype
+
+      -- Disable autoformat if global or buffer-local variable is set
       if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
         return
       end
-      -- Disable autoformat for files in a certain path
-      local bufname = vim.api.nvim_buf_get_name(bufnr)
+
+      -- Disable autoformat for files in node_modules
       if bufname:match("/node_modules/") then
         return
       end
-      -- ...additional logic...
+
+      -- For JavaScript and TypeScript files, check for .prettierrc.json in git root
+      if
+        filetype == "javascript"
+        or filetype == "typescript"
+        or filetype == "javascriptreact"
+        or filetype == "typescriptreact"
+      then
+        -- Find git root directory
+        local git_root = nil
+        local current_dir = vim.fn.fnamemodify(bufname, ":p:h")
+
+        -- Function to find git root
+        local function find_git_root(dir)
+          local git_dir = dir .. "/.git"
+          if vim.fn.isdirectory(git_dir) == 1 then
+            return dir
+          end
+
+          -- Go up one directory
+          local parent = vim.fn.fnamemodify(dir, ":h")
+          if parent == dir then
+            -- We've reached the filesystem root
+            return nil
+          end
+
+          return find_git_root(parent)
+        end
+
+        git_root = find_git_root(current_dir)
+
+        -- Check for .prettierrc.json in git root if we found a git root
+        if git_root then
+          local prettier_path = git_root .. "/.prettierrc.json"
+          if vim.fn.filereadable(prettier_path) == 1 then
+            return { timeout_ms = 1000, lsp_fallback = true }
+          end
+        end
+
+        -- Don't format JS/TS if no .prettierrc.json found in git root
+        return
+      end
+
+      -- For all other filetypes
       return { timeout_ms = 1000, lsp_fallback = true }
     end,
     formatters_by_ft = {
@@ -55,3 +96,4 @@ return {
     },
   },
 }
+
