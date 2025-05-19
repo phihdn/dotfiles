50cc5db27efb7df2fa9168cba4438b4a2020c9db
Phi <phihdn@gmail.com>
2025-05-18 22:16:21 -0300

    NVIM add Treesitter

--- nvim/conf/lua/plugins/kanagawa.lua
+++ nvim/conf/lua/plugins/kanagawa.lua
@@ -1,13 +1,22 @@
 return {
   "rebelot/kanagawa.nvim",
-  config=function() 
+  branch="master",
+  config=function()
     require('kanagawa').setup({
-      compile=true,
       transparent=true,
+      -- coloring by Treesitter, use :Inspect to get the selector and link to the theme options
+      overrides=function(colors)
+        return {
+          ["@markup.link.url.markdown_inline"] = { link = "Special" }, -- (url)
+          ["@markup.link.label.markdown_inline"] = { link = "WarningMsg" }, -- [label]
+          ["@markup.italic.markdown_inline"] = { link = "Exception" }, -- *italic*
+          ["@markup.raw.markdown_inline"] = { link = "String" }, -- `code`
+          ["@markup.list.markdown"] = { link = "Function" }, -- + list
+          ["@markup.quote.markdown"] = { link = "Error" }, -- > blockcode
+          ["@markup.list.checked.markdown"] = { link = "WarningMsg" } -- - [X] checked list item
+        }
+      end
     });
     vim.cmd("colorscheme kanagawa");
-  end,
-  build = function()
-    vim.cmd("KanagawaCompile");
   end,
 }
