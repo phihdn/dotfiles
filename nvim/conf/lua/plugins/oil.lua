cea61d8aec7ea6081026508876e6860c1ddaa8c5
Phi <phihdn@gmail.com>
2025-05-18 22:44:05 -0300

    NVIM show hidden files in oil

--- nvim/conf/lua/plugins/oil.lua
+++ nvim/conf/lua/plugins/oil.lua
@@ -1,8 +1,12 @@
 return {
-  'stevearc/oil.nvim',
+  "stevearc/oil.nvim",
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+  },
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
